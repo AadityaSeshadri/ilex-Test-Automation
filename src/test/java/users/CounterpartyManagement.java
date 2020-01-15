85d443c643e3944dffe1c00ca7372bedf6bbf7fd
Aaditya Seshadri <aseshadri@palo-it.com>
2020-01-15 16:10:36 +0800

    Code added for Counterparty Management

--- src/test/java/users/CounterpartyManagement.java
+++ src/test/java/users/CounterpartyManagement.java
@@ -1,9 +1,9 @@
-package examples.users;
+package users;
 
 import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
-public class UsersRunner {
+public class CounterpartyManagement {
 
 }
